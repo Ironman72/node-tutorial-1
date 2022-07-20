29bd6fac77bef0abc4e287ec043ae1a555ec276f
Ironman72 <jarvisismyheart@gmail.com>
2022-07-20 12:44:01 +0530

    car object creating & exporting

---
+++ car.js
@@ -0,0 +1,8 @@
+//creating a car object..
+const car = {  
+    name : 'BMW',
+    model : '330i',
+    year : 2021
+}
+
+module.exports = car; // exporting the car object.
---
+++ magic.js
@@ -0,0 +1,3 @@
+const car = require('./car.js');  //importing the car object that created in car.js file
+
+console.log(car.name)   //printing the car name.. (car) holds all the details of car object
